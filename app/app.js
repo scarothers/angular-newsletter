9d2a8b1c3518dc82bc4705f26bfb8ebea41155cd
scarothers <saracarothers@gmail.com>
2015-12-16 21:26:16 -0500

    Add docker-compose.yml

--- app/app.js
+++ app/app.js
@@ -4,7 +4,9 @@ app.controller('StoriesController', function ($scope, $http){
 	//For prod server:
 	//callurl = 'http://52.26.239.23:5000/'
 	//For local development:
-	callurl = 'http://0.0.0.0:5000/'
+	//callurl = 'http://0.0.0.0:5000/'
+	//For Docker development: 
+	callurl = 'http://192.168.99.100:5000/'
 	$http.get(callurl)
 	//$http.get('data.json')
 	.then(function(res){
